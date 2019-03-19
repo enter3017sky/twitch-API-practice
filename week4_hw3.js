c38090aa36e5b2f8f4fc9ea00dc65c6a5aa9f767
enter3017sky <enter3017sky@gmail.com>
2019-03-19 19:36:30 +0800

    update

--- week4_hw3.js
+++ week4_hw3.js
@@ -1,51 +1,25 @@
-// curl -H 'Accept: application/vnd.twitchtv.v5+json' \
-// -H 'Client-ID: uo6dggojyb8d6soh92zknwmi5ej1q2' \
-// -X GET 'https://api.twitch.tv/kraken/streams/?game=Overwatch'
-// var apiUrl = 'https://api.twitch.tv/kraken/streams/?clientId=' + clientId + '&game=League%20of%20Legends&limit=' + limit ;
-// https://api.twitch.tv/kraken/streams?game=League%20of%20Legends&limit=2&client_id=eq990vt85o5dquacakpy5u32ofrmmt
-
-// 把資料寫成變數，可讀性高，更改資料方便
-// var clientId = 'eq990vt85o5dquacakpy5u32ofrmmt';
-// var limit = 2;
-// var apiUrl = 'https://api.twitch.tv/kraken/streams/?client_id=' + clientId + '&game=League%20of%20Legends&limit=' + limit ;
-
-// // 我要一個新的 request , 首先 new 一個 XMLHttpRequest() 出來
-// let request = new XMLHttpRequest();
-// // 送到哪裡去
-// request.open('GET', apiUrl, true);
-
-// // 自訂義的標頭拿掉的話，Request 就會剩一個，就不會 Preflight Request 了
-// request.setRequestHeader('client-id', clientId)
-// request.send();
-// // 加一個 callback function
-// request.onload = function() {
-//     if (request.status >= 200 && request.status < 400) {
-//         var data = JSON.parse(this.responseText);
-//         console.log(data);
-//     }
-// }
-
-
-var request = new XMLHttpRequest();
 var clientId = 'eq990vt85o5dquacakpy5u32ofrmmt';
 var limit = 20;
-var apiUrl = 'https://api.twitch.tv/kraken/streams/?client_id=' + clientId + '&game=League%20of%20Legends&limit=' + limit ;
-request.open('GET', apiUrl, true);
-request.onload = function() {
-    if (request.status === 200) {
-        // 成功的話 處理資料
-        var data = JSON.parse(request.responseText);
+var apiUrl = 'https://api.twitch.tv/kraken/streams/?client_id=' + clientId + '&game=League%20of%20Legends&limit=' + limit;
+
+// Create XHR Object
+var xhr = new XMLHttpRequest();
+
+// OPEN - type, url/file, async
+xhr.open('GET', apiUrl, true);
+
+console.log('Ready State: ', xhr.readyState)
+
+// OPTIONAL - used for loaders
+xhr.onprogress = function() {
+    console.log('Ready State: ', xhr.readyState)
+}
+
+xhr.onload = function() {
+console.log('Ready State: ', xhr.readyState)
+    if (this.status === 200) {
+        var data = JSON.parse(xhr.responseText);
         var cols = data.streams;
-        // console.log(data)
-        // console.log(cols[0]) // 有 index 才取得到個別的資料
-        // console.log(cols[0].channel.display_name)
-        // console.log(cols[0].channel.name)
-        // console.log(cols[0].preview.medium)
-        // console.log(cols[0].channel.status)
-        // // console.log(cols[0].channel.views)
-        // console.log(cols[0].channel.logo) 
-
-
         var row = document.querySelector('.row');
         var result = '';
         for (var i = 0; i < cols.length; i++) { // 將內容先處理好
@@ -54,7 +28,9 @@ request.onload = function() {
                 <a href="${cols[i].channel.url}" target="_blank">
                     <div class='col'>
                         <div class='preview'>
-                            <img src="${cols[i].preview.medium}" />
+                            <div class="placeholder"></div>
+                            <img src="${cols[i].preview.medium}" 
+                            onload= "this.style.opacity=1;"/>
                         </div>
                         <div class='bottom'>
                             <div class='bottom__avatar'>
@@ -81,8 +57,9 @@ request.onload = function() {
     }
 };
 
-request.onerror = function() {
+xhr.onerror = function() {
   // There was a connection error of some sort
+  console.log('Request error...')
 };
 
-request.send();
+xhr.send();
